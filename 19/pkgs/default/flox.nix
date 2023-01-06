137aaf4065a803be9082c0e620fb600602fa2886
Robin Brantley <rtb@floxdev.com>
2023-01-06 11:22:42 +0000

    brantley upgraded
    flox upgrade

---
+++ 19/pkgs/default/flox.nix
@@ -0,0 +1,25 @@
+{...}: {
+  packages = {
+    nixpkgs-flox = {
+      # hello = {version = "2.12.1";};
+    };
+  };
+  packages.nixpkgs-flox.git = {};
+  packages.nixpkgs-flox.jq = {};
+  packages.nixpkgs-flox.asciinema = {};
+  packages.nixpkgs-flox.asciinema-agg = {stability="staging";};
+  packages.nixpkgs-flox.ack = {};
+  packages.nixpkgs-flox.tmux = {};
+  shell = {
+    aliases = {
+      # gg = "git grep";
+    };
+    # POSIX shell hook, except curly braces have to be escaped like ``${
+    hook = ''
+      # echo "Supercharged by flox!" 1>&2
+    '';
+  };
+  environmentVariables = {
+    # LANG = "en_US.UTF-8";
+  };
+}
