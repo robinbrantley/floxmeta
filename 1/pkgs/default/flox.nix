92fd7e133886c6c9b670885ae61e12e2b5f672a7
Robin Brantley <floxuser@example.invalid>
2023-02-04 12:00:43 +0100

    rtb installed stable.nixpkgs-flox.git
    flox install git
      stable.nixpkgs-flox.git: ∅ -> 2.38.1

---
+++ 1/pkgs/default/flox.nix
@@ -0,0 +1,21 @@
+{
+  packages = {
+    nixpkgs-flox = {
+      # ripgrep = {version = "13.0.0";};
+    };
+  };
+  packages.nixpkgs-flox.git = {};
+  shell = {
+    aliases = {
+      # gg = "git grep";
+    };
+    # POSIX shell hook run by flox activate, except curly braces have to be
+    # escaped like ''${
+    hook = ''
+      # echo "Supercharged by flox!" 1>&2
+    '';
+  };
+  environmentVariables = {
+    # LANG = "en_US.UTF-8";
+  };
+}
